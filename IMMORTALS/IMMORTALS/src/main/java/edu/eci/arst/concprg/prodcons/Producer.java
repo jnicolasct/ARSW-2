82a576b9f8b58b380229deb48a32eef19765523a
unknown <andres.quintero-d@labinfo.is.escuelaing.edu.co>
2020-01-30 10:50:28 -0500

    punto 3

--- IMMORTALS/IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ IMMORTALS/IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,16 +31,16 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
-
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            synchronized (queue) {
-                queue.add(dataSeed);
-                queue.notify();
+            if (queue.size() < stockLimit) {
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                synchronized (queue) {
+                    queue.add(dataSeed);
+                    queue.notify();
+                }
             }
-
             try {
-                Thread.sleep(1000);
+                Thread.sleep(500);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
             }
