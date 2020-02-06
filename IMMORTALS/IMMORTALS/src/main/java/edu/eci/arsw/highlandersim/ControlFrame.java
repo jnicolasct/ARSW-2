4721b09cb877cf89425bf6e486dfe5082e4995c3
Andres Quintero <andres.quintero98@hotmail.com>
2020-02-05 19:26:45 -0500

    parte 2

--- IMMORTALS/IMMORTALS/src/main/java/edu/eci/arsw/highlandersim/ControlFrame.java
+++ IMMORTALS/IMMORTALS/src/main/java/edu/eci/arsw/highlandersim/ControlFrame.java
@@ -20,6 +20,8 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JScrollBar;
 
 public class ControlFrame extends JFrame {
@@ -89,16 +91,21 @@ public class ControlFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
 
                 /*
-				 * COMPLETAR
+                 * COMPLETAR
                  */
-                int sum = 0;
-                for (Immortal im : immortals) {
-                    sum += im.getHealth();
-                }
+                synchronized (immortals) {
+                    for (Immortal im : immortals) {
+                        
+                        im.cambieEstado(1);
+                    }
 
-                statisticsLabel.setText("<html>"+immortals.toString()+"<br>Health sum:"+ sum);
-                
-                
+                    int sum = 0;
+                    for (Immortal im : immortals) {
+                    
+                        sum += im.getHealth();
+                    }
+                    statisticsLabel.setText("<html>" + immortals.toString() + "<br>Health sum:" + sum);
+                }
 
             }
         });
@@ -108,9 +115,10 @@ public class ControlFrame extends JFrame {
 
         btnResume.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                /**
-                 * IMPLEMENTAR
-                 */
+                
+                for (Immortal im : immortals) {
+                    im.cambieEstado(0);
+                }
 
             }
         });
@@ -126,6 +134,13 @@ public class ControlFrame extends JFrame {
         numOfImmortals.setColumns(10);
 
         JButton btnStop = new JButton("STOP");
+        btnStop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                btnStart.setEnabled(true);
+                output.setText("");
+
+            }
+        });
         btnStop.setForeground(Color.RED);
         toolBar.add(btnStop);
 
@@ -135,8 +150,7 @@ public class ControlFrame extends JFrame {
         output = new JTextArea();
         output.setEditable(false);
         scrollPane.setViewportView(output);
-        
-        
+
         statisticsLabel = new JLabel("Immortals total health:");
         contentPane.add(statisticsLabel, BorderLayout.SOUTH);
 
@@ -144,15 +158,15 @@ public class ControlFrame extends JFrame {
 
     public List<Immortal> setupInmortals() {
 
-        ImmortalUpdateReportCallback ucb=new TextAreaUpdateReportCallback(output,scrollPane);
-        
+        ImmortalUpdateReportCallback ucb = new TextAreaUpdateReportCallback(output, scrollPane);
+
         try {
             int ni = Integer.parseInt(numOfImmortals.getText());
 
             List<Immortal> il = new LinkedList<Immortal>();
 
             for (int i = 0; i < ni; i++) {
-                Immortal i1 = new Immortal("im" + i, il, DEFAULT_IMMORTAL_HEALTH, DEFAULT_DAMAGE_VALUE,ucb);
+                Immortal i1 = new Immortal("im" + i, il, DEFAULT_IMMORTAL_HEALTH, DEFAULT_DAMAGE_VALUE, ucb);
                 il.add(i1);
             }
             return il;
@@ -165,16 +179,16 @@ public class ControlFrame extends JFrame {
 
 }
 
-class TextAreaUpdateReportCallback implements ImmortalUpdateReportCallback{
+class TextAreaUpdateReportCallback implements ImmortalUpdateReportCallback {
 
     JTextArea ta;
     JScrollPane jsp;
 
-    public TextAreaUpdateReportCallback(JTextArea ta,JScrollPane jsp) {
+    public TextAreaUpdateReportCallback(JTextArea ta, JScrollPane jsp) {
         this.ta = ta;
-        this.jsp=jsp;
-    }       
-    
+        this.jsp = jsp;
+    }
+
     @Override
     public void processReport(String report) {
         ta.append(report);
@@ -189,5 +203,5 @@ class TextAreaUpdateReportCallback implements ImmortalUpdateReportCallback{
         );
 
     }
-    
+
 }
--- IMMORTALS/IMMORTALS/src/main/java/edu/eci/arsw/highlandersim/Immortal.java
+++ IMMORTALS/IMMORTALS/src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -2,13 +2,15 @@ package edu.eci.arsw.highlandersim;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Immortal extends Thread {
 
-    private ImmortalUpdateReportCallback updateCallback=null;
-    
+    private ImmortalUpdateReportCallback updateCallback = null;
+
     private int health;
-    
+
     private int defaultDamageValue;
 
     private final List<Immortal> immortalsPopulation;
@@ -17,50 +19,62 @@ public class Immortal extends Thread {
 
     private final Random r = new Random(System.currentTimeMillis());
 
+    private Boolean continuar;
 
     public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
         super(name);
-        this.updateCallback=ucb;
+        this.updateCallback = ucb;
         this.name = name;
         this.immortalsPopulation = immortalsPopulation;
         this.health = health;
-        this.defaultDamageValue=defaultDamageValue;
+        this.defaultDamageValue = defaultDamageValue;
+        this.continuar = true;
     }
 
     public void run() {
 
-        while (true) {
+        while (getHealth()>0) {
             Immortal im;
-
-            int myIndex = immortalsPopulation.indexOf(this);
-
-            int nextFighterIndex = r.nextInt(immortalsPopulation.size());
-
-            //avoid self-fight
-            if (nextFighterIndex == myIndex) {
-                nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
+            synchronized (this) {
+                if (!continuar) {
+                    try {
+                        wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Immortal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
+            synchronized (immortalsPopulation){
+                int myIndex = immortalsPopulation.indexOf(this);
 
-            im = immortalsPopulation.get(nextFighterIndex);
+                int nextFighterIndex = r.nextInt(immortalsPopulation.size());
 
-            this.fight(im);
+                //avoid self-fight
+                if (nextFighterIndex == myIndex) {
+                    nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
+                }
 
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                im = immortalsPopulation.get(nextFighterIndex);
+                
+                if (immortalsPopulation.get(nextFighterIndex).getHealth()>0){
+                    this.fight(im);
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-
         }
 
     }
 
-    public void fight(Immortal i2) {
+    public synchronized void fight(Immortal i2) {
 
         if (i2.getHealth() > 0) {
             i2.changeHealth(i2.getHealth() - defaultDamageValue);
             this.health += defaultDamageValue;
-            updateCallback.processReport("Fight: " + this + " vs " + i2+"\n");
+            updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
         } else {
             updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
         }
@@ -79,6 +93,18 @@ public class Immortal extends Thread {
     public String toString() {
 
         return name + "[" + health + "]";
+    }
+
+    public void cambieEstado(int valor) {
+        if (valor == 1) {
+            continuar = false;
+        } else {
+            continuar = true;
+            synchronized(this){
+                notifyAll();
+            }
+        }
+
     }
 
 }
